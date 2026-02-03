7073afc0ee0a67a57991a7a1e190b5fa251a9e05
Jonathan Gilbert <logic@deltaq.org>
2026-02-03 15:51:42 -0600

    Added the ability to interrupt waits for keyboard input in Keyboard.cs: - Added fields _waitCount and _interruptCount. - Updated all of the Wait overloads to increment _waitCount on entry and decrement it on exit. - Updated all of the Wait overloads to check if _interruptCount is non-zero before entering and after each wait operation, and if it is, to decrement it and bail. - Added method InterruptWait that sets _interruptCount to _waitCount and pulses all waiters (does nothing if a previous call is still being processed).

--- QBX/Hardware/Keyboard.cs
+++ QBX/Hardware/Keyboard.cs
@@ -142,14 +142,33 @@ public class Keyboard(Machine machine)
 
 		lock (_sync)
 		{
+			// Prevent entering the loop while other threads are in the process of being
+			// interrupted, otherwise we might eat their interruption notification.
+			if (_interruptCount > 0)
+				return false;
+
 			EatReleaseEvents(_inputQueue);
 
-			while (machine.KeepRunning && (_inputQueue.Count == 0))
-			{
-				Monitor.Wait(_sync);
-				EatReleaseEvents(_inputQueue);
-			}
+			Interlocked.Increment(ref _waitCount);
 
+			try
+			{
+				while (machine.KeepRunning && (_inputQueue.Count == 0))
+				{
+					Monitor.Wait(_sync);
+
+					if (_interruptCount > 0)
+						return true;
+
+					EatReleaseEvents(_inputQueue);
+				}
+			}
+			finally
+			{
+				Interlocked.Decrement(ref _waitCount);
+				if (_interruptCount > 0)
+					Interlocked.Decrement(ref _interruptCount);
+			}
 
 			return machine.KeepRunning;
 		}
@@ -161,18 +180,81 @@ public class Keyboard(Machine machine)
 
 		lock (_sync)
 		{
+			// Prevent entering the loop while other threads are in the process of being
+			// interrupted, otherwise we might eat their interruption notification.
+			if (_interruptCount > 0)
+				return false;
+
 			EatReleaseEvents(_inputQueue);
 
-			while (_inputQueue.Count == 0)
+			Interlocked.Increment(ref _waitCount);
+
+			try
 			{
-				var remainingTime = deadline - DateTime.UtcNow;
+				while (_inputQueue.Count == 0)
+				{
+					var remainingTime = deadline - DateTime.UtcNow;
 
-				if (remainingTime <= TimeSpan.Zero)
-					return false;
+					if (remainingTime <= TimeSpan.Zero)
+						return false;
 
-				Monitor.Wait(_sync, remainingTime);
+					Monitor.Wait(_sync, remainingTime);
 
-				EatReleaseEvents(_inputQueue);
+					if (_interruptCount > 0)
+						return false;
+
+					EatReleaseEvents(_inputQueue);
+				}
+			}
+			finally
+			{
+				Interlocked.Decrement(ref _waitCount);
+				if (_interruptCount > 0)
+					Interlocked.Decrement(ref _interruptCount);
+			}
+
+			return true;
+		}
+	}
+
+	public bool WaitForInput(CancellationToken cancellationToken)
+	{
+		void NotifyWaitLoop()
+		{
+			lock (_sync)
+				Monitor.PulseAll(_sync);
+		}
+
+		using (cancellationToken.Register(NotifyWaitLoop))
+		lock (_sync)
+		{
+			// Prevent entering the loop while other threads are in the process of being
+			// interrupted, otherwise we might eat their interruption notification.
+			if (_interruptCount > 0)
+				return false;
+
+			Interlocked.Increment(ref _waitCount);
+
+			try
+			{
+				while (_inputQueue.Count == 0)
+				{
+					Monitor.Wait(_sync);
+
+					if (_interruptCount > 0)
+						return false;
+
+					EatReleaseEvents(_inputQueue);
+
+					if (cancellationToken.IsCancellationRequested)
+						return false;
+				}
+			}
+			finally
+			{
+				Interlocked.Decrement(ref _waitCount);
+				if (_interruptCount > 0)
+					Interlocked.Decrement(ref _interruptCount);
 			}
 
 			return true;
@@ -189,13 +271,23 @@ public class Keyboard(Machine machine)
 			if (_divertEvents)
 				throw new InvalidOperationException("WaitForNextInput does not support concurrent operation");
 
+			// Prevent entering the loop while other threads are in the process of being
+			// interrupted, otherwise we might eat their interruption notification.
+			if (_interruptCount > 0)
+				return false;
+
 			_divertEvents = true;
+
+			Interlocked.Increment(ref _waitCount);
 
 			try
 			{
 				while (_divertedEvents.Count == 0)
 				{
 					Monitor.Wait(_sync);
+
+					if (_interruptCount > 0)
+						return false;
 
 					EatReleaseEvents(_divertedEvents);
 				}
@@ -207,6 +299,10 @@ public class Keyboard(Machine machine)
 			}
 			finally
 			{
+				Interlocked.Decrement(ref _waitCount);
+				if (_interruptCount > 0)
+					Interlocked.Decrement(ref _interruptCount);
+
 				_divertEvents = false;
 				_divertedEvents.Clear();
 			}
@@ -222,7 +318,14 @@ public class Keyboard(Machine machine)
 			if (_divertEvents)
 				throw new InvalidOperationException("WaitForNextInput does not support concurrent operation");
 
+			// Prevent entering the loop while other threads are in the process of being
+			// interrupted, otherwise we might eat their interruption notification.
+			if (_interruptCount > 0)
+				return false;
+
 			_divertEvents = true;
+
+			Interlocked.Increment(ref _waitCount);
 
 			try
 			{
@@ -235,6 +338,9 @@ public class Keyboard(Machine machine)
 
 					Monitor.Wait(_sync, remainingTime);
 
+					if (_interruptCount > 0)
+						return false;
+
 					EatReleaseEvents(_divertedEvents);
 				}
 
@@ -245,35 +351,29 @@ public class Keyboard(Machine machine)
 			}
 			finally
 			{
+				Interlocked.Decrement(ref _waitCount);
+				if (_interruptCount > 0)
+					Interlocked.Decrement(ref _interruptCount);
+
 				_divertEvents = false;
 				_divertedEvents.Clear();
 			}
 		}
 	}
 
-	public bool WaitForInput(CancellationToken cancellationToken)
-	{
-		void NotifyWaitLoop()
-		{
-			lock (_sync)
-				Monitor.PulseAll(_sync);
-		}
+	volatile int _waitCount;
+	volatile int _interruptCount;
 
-		using (cancellationToken.Register(NotifyWaitLoop))
+	public void InterruptWait()
+	{
 		lock (_sync)
 		{
-			while (_inputQueue.Count == 0)
-			{
-				Monitor.Wait(_sync);
+			if (_interruptCount > 0)
+				return;
 
-				while ((_inputQueue.Count > 0) && _inputQueue.Peek().IsRelease)
-					_inputQueue.Dequeue();
+			_interruptCount = _waitCount;
 
-				if (cancellationToken.IsCancellationRequested)
-					return false;
-			}
-
-			return true;
+			Monitor.PulseAll(_sync);
 		}
 	}
 
