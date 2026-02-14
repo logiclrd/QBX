ce5b30cde573d1219e3a7753b6c7941da48671ab
Jonathan Gilbert <logic@deltaq.org>
2026-02-14 00:30:52 -0600

    Added device FileDescriptor type ClockFileDescriptor.cs. Added field Clock to Devices.cs and updated the constructor to include it in the _devices map.

--- QBX/OperatingSystem/Devices.cs
+++ QBX/OperatingSystem/Devices.cs
@@ -9,6 +9,7 @@ public class Devices
 {
 	Dictionary<string, FileDescriptor> _devices = new Dictionary<string, FileDescriptor>(StringComparer.InvariantCultureIgnoreCase);
 
+	public readonly ClockFileDescriptor Clock;
 	public readonly ConsoleFileDescriptor Console;
 	public readonly NullFileDescriptor Null;
 
@@ -17,9 +18,11 @@ public class Devices
 
 	public Devices(DOS owner)
 	{
+		Clock = new ClockFileDescriptor();
 		Console = new ConsoleFileDescriptor(owner);
 		Null = new NullFileDescriptor();
 
+		_devices["CLOCK$"] = Clock;
 		_devices["CON"] = Console;
 		_devices["NUL"] = Null;
 	}
---
+++ QBX/OperatingSystem/FileDescriptors/ClockFileDescriptor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using QBX.OperatingSystem.FileStructures;
+using QBX.Parser;
+
+namespace QBX.OperatingSystem.FileDescriptors;
+
+public class ClockFileDescriptor : FileDescriptor
+{
+	protected override bool CanRead => true;
+	protected override bool CanWrite => true;
+
+	protected override uint SeekCore(int offset, MoveMethod moveMethod) => 0;
+	protected override uint SeekCore(uint offset, MoveMethod moveMethod) => 0;
+
+	// Observed behaviour on DOS 6 in a VM
+	static byte[] Fill = { 0xCE, 0xCE, 0xCE, 0xCE, 0xCE, 0xCE, 0xCE, 0xCE, 0xCE, 0xCE, 0xCE, 0xCE, 0xCE, 0xCE, 0xCE, 0xCE };
+
+	protected override void ReadCore(FileBuffer buffer)
+	{
+		buffer.Push(Fill.AsSpan().Slice(0, Math.Min(Fill.Length, buffer.Available)));
+	}
+
+	// TODO: What is the correct behaviour here?
+	protected override int WriteCore(ReadOnlySpan<byte> buffer) => buffer.Length;
+}
