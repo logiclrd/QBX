62c27ee5d38dcc256c103762155c8bee8efd5c3a
Jonathan Gilbert <logic@deltaq.org>
2026-03-07 16:40:41 -0600

    Corrected typo in ConstructibleFunction.cs.

--- QBX/ExecutionEngine/Compiled/Functions/ConstructibleFunction.cs
+++ QBX/ExecutionEngine/Compiled/Functions/ConstructibleFunction.cs
@@ -6,6 +6,6 @@ public abstract class ConstructibleFunction : Function
 {
 	protected sealed override void SetArgument(int index, Evaluable value)
 	{
-		throw new Exception("Constructing function type " + GetType().Name + " does not support setting argument(s) after construction");
+		throw new Exception("Constructible function type " + GetType().Name + " does not support setting argument(s) after construction");
 	}
 }
