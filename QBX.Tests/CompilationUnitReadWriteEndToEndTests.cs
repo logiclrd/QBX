2b56174ea30b782796d016651049a480cf4101dd
Jonathan Gilbert <logic@deltaq.org>
2026-03-31 18:47:46 -0500

    Added new tests file CompilationUnitReadWriteEndToEndTests.cs. Rewrote KALEID.BAS, MAZERUN.BAS, NIBBLESX.BAS and PLASMA.BAS with QuickBASIC 7.1. Rewrote PIPELINE.BAS with the new declaration-generating QBX code.

---
+++ QBX.Tests/CompilationUnitReadWriteEndToEndTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.CompilerServices;
+
+using QBX.CodeModel;
+using QBX.LexicalAnalysis;
+using QBX.Parser;
+using QBX.Tests.Utility;
+
+namespace QBX.Tests;
+
+public class CompilationUnitReadWriteEndToEndTests
+{
+	static string GetSamplesPath([CallerFilePath] string testFilePath = "")
+	{
+		string testProjectPath = Path.GetDirectoryName(testFilePath)!;
+		string solutionPath = Path.GetDirectoryName(testProjectPath)!;
+
+		return Path.Combine(solutionPath, "Samples");
+	}
+
+	public static IEnumerable<string> EnumerateSamples()
+	{
+		string samplesPath = GetSamplesPath();
+
+		return Directory.EnumerateFiles(samplesPath, "*.BAS");
+	}
+
+	[TestCaseSource(nameof(EnumerateSamples))]
+	public void TestParseAndFormat(string path)
+	{
+		// Arrange
+		var sourceCode = File.ReadAllText(path);
+
+		using (var reader = new StringReader(sourceCode))
+		{
+			var formattedBuffer = new StringWriter() { NewLine = "\r\n" };
+
+			// Act
+			var parsed = CompilationUnit.Read(reader, path, ignoreErrors: true);
+
+			parsed.PrepareForWrite();
+			parsed.Write(formattedBuffer);
+
+			var formatted = formattedBuffer.ToString();
+
+			// Assert
+
+			// Ignore trailing whitespace and newlines
+			formatted = formatted.RemoveTrailingWhitespace();
+			sourceCode = sourceCode.RemoveTrailingWhitespace();
+
+			formatted.Should().Be(sourceCode);
+		}
+	}
+}
