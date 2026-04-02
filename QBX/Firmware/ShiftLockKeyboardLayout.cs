4fe52cb974ae476c93a494154d632b172985bd6a
Jonathan Gilbert <logic@deltaq.org>
2026-04-02 13:56:20 -0500

    Added a German keyboard layout: - Added intermediate base class ShiftLockKeyboardLayout.cs to Firmware. - Added KeyboardLayouts file GR.cs implementing the state transition logic observed with the MS-DOS "GR" keyboard layout.

---
+++ QBX/Firmware/ShiftLockKeyboardLayout.cs
@@ -0,0 +1,22 @@
+﻿using QBX.Hardware;
+
+using SDL3;
+
+namespace QBX.Firmware;
+
+public abstract class ShiftLockKeyboardLayout(Machine machine) : KeyboardLayout(machine)
+{
+	protected override void UpdateCapsLockState(RawKeyEventData data, ref int keyboardStatus)
+	{
+		switch (data.RawScanCode)
+		{
+			case SDL.Scancode.Capslock:
+				keyboardStatus |= SystemMemory.KeyboardStatus_CapsLockBit;
+				break;
+			case SDL.Scancode.LShift:
+			case SDL.Scancode.RShift:
+				keyboardStatus &= ~SystemMemory.KeyboardStatus_CapsLockBit;
+				break;
+		}
+	}
+}
