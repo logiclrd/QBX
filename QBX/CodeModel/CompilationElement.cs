f9389b70244a2056a6deb002909f2fe2927cb88b
Jonathan Gilbert <logic@deltaq.org>
2026-09-10 18:02:26 -0500

    Updated the SourceLineIndex reindexing in ReindexLines and AddLine in CompilationElement.cs to factor in the CompilationElement's FirstLineIndex.

--- QBX/CodeModel/CompilationElement.cs
+++ QBX/CodeModel/CompilationElement.cs
@@ -98,7 +98,7 @@ public class CompilationElement : IRenderableCode, IEditableElement
 	void ReindexLines(int startIndex)
 	{
 		for (int lineIndex = startIndex; lineIndex < _lines.Count; lineIndex++)
-			_lines[lineIndex].SourceLineIndex.Value = lineIndex;
+			_lines[lineIndex].SourceLineIndex.Value = lineIndex + FirstLineIndex;
 	}
 
 	void IEditableElement.AddLine(IEditableLine line) => AddLine((CodeLine)line);
@@ -106,7 +106,7 @@ public class CompilationElement : IRenderableCode, IEditableElement
 	public void AddLine(CodeLine line)
 	{
 		line.CompilationElement = this;
-		line.SourceLineIndex.Value = _lines.Count;
+		line.SourceLineIndex.Value = _lines.Count + FirstLineIndex;
 
 		_lines.Add(line);
 	}
