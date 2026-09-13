2c90746965cffe359aa8f25fb91882c272676ee2
Jonathan Gilbert <logic@deltaq.org>
2026-09-13 18:28:15 -0500

    Updated RestartExecutionFromLineStatement.cs to pass its source statement information into the ReplaceRunningProgram constructor for error context.

--- QBX/ExecutionEngine/Compiled/Statements/RestartExecutionFromLineStatement.cs
+++ QBX/ExecutionEngine/Compiled/Statements/RestartExecutionFromLineStatement.cs
@@ -12,6 +12,6 @@ public class RestartExecutionFromLineStatement(Identifier startingLineNumber, Co
 		if (TargetPath == null)
 			throw RuntimeException.LabelNotDefined(source);
 
-		throw new ReplaceRunningProgram(TargetPath);
+		throw new ReplaceRunningProgram(TargetPath, source);
 	}
 }
