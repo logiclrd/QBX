21a231ff2fcfa02470909aa739be4192e164a026
Jonathan Gilbert <logic@deltaq.org>
2026-09-16 20:35:16 -0500

    Reworked AppDomain_FirstChangeException in DebugExceptionHelper.cs to be a lot more picky about which exception types it breaks on.

--- QBX/DebugExceptionHelper.cs
+++ QBX/DebugExceptionHelper.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Runtime.ExceptionServices;
 
+using QBX.ExecutionEngine;
+using QBX.ExecutionEngine.Compiled.Statements;
+using QBX.ExecutionEngine.Execution;
 using QBX.Parser;
 
 namespace QBX;
@@ -21,25 +25,78 @@ class DebugExceptionHelper
 
 	private static void AppDomain_FirstChanceException(object? sender, FirstChanceExceptionEventArgs e)
 	{
-		if (e.Exception is SyntaxErrorException)
+		bool suppress;
+
+		if (e.Exception.Message.StartsWith("Internal"))
+			suppress = false;
+		else
 		{
-			bool suppress = false;
+			suppress = false;
 
-			foreach (var frame in new StackTrace().GetFrames())
+			bool scanForParserTestMethod = false;
+			bool scanForIDELoadMethod = false;
+
+			switch (e.Exception)
 			{
-				var method = frame.GetMethod();
-
-				if ((method != null)
-				 && (method.DeclaringType == typeof(BasicParser))
-				 && method.Name.StartsWith("Test"))
-				{
+				case GoTo:
+				case Resume:
+				case ExitDo:
+				case ExitFor:
+				case ExitScope:
+				case BreakExecution:
+				case EndProgram:
+				case TerminatedException:
 					suppress = true;
 					break;
-				}
+
+				case SyntaxErrorException:
+					scanForParserTestMethod = true;
+					break;
+				case IOException:
+					scanForIDELoadMethod = true;
+					break;
+				case RuntimeException error:
+					switch (error.ErrorNumber)
+					{
+						case 12: // Invalid access
+						case 53: // File not found
+						case 57: // Device IO error
+						case 67: // Too many open files
+						case 75: // Path/file access error
+						case 76: // Path not found
+							scanForIDELoadMethod = true;
+							break;
+					}
+
+					break;
 			}
 
-			if (!suppress)
-				Debugger.Break();
+			if (scanForParserTestMethod || scanForIDELoadMethod)
+			{
+				foreach (var frame in new StackTrace().GetFrames())
+				{
+					var method = frame.GetMethod();
+
+					if (method != null)
+					{
+						if (scanForParserTestMethod
+						 && (method.DeclaringType == typeof(BasicParser))
+						 && method.Name.StartsWith("Test"))
+							suppress = true;
+
+						if (scanForIDELoadMethod
+						 && (method.DeclaringType == typeof(DevelopmentEnvironment.Program))
+						 && method.Name.StartsWith("Load"))
+							suppress = true;
+
+						if (suppress)
+							break;
+					}
+				}
+			}
 		}
+
+		if (!suppress)
+			Debugger.Break();
 	}
 }
